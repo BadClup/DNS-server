90e45ca92ca766c9077c33e7dcd575a180090198
Bartłomiej Strama <kontakt@bstrama.com>
2022-07-06 22:31:06 +0200

    localhost/login POST method added

--- client/routes/auth.router.js
+++ client/routes/auth.router.js
@@ -1,10 +1,33 @@
-const express = require('express');
+const path = require('path'),
+
+    express = require('express'),
+
+    accounts = require('../models/mongodb.model');
+
 
 const login = express.Router()
-
 login.route('/')
     .post((req, res) => {
+        const body = req.body;
 
+        accounts.findOne({
+            name: body.login,
+            password: body.password
+        })
+            .then(data => {
+                if (data) {
+                    res.status(200).json({
+                        status: 200,
+                        areLoggedIn:true
+                    })
+                } else {
+                    res.status(400).json({
+                        error: "bad login or password",
+                        status: 400,
+                        areLoggedIn: false
+                    })
+                }
+            })
     })
 
 module.exports = {
