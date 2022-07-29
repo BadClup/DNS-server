e95591e6bffc5bffeedd9db5cca2be03c3792777
Bartłomiej Strama <kontakt@bstrama.com>
2022-07-29 21:48:59 +0200

    small mongo-login changes

--- mongo-login.js
+++ mongo-login.js
@@ -4,10 +4,15 @@ const fs = require('fs'),
 const argv = process.argv.slice(2);
 
 if (argv.length === 2) {
-    const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')).toString());
-    config.dbUrl = `mongodb+srv://${argv[0]}:${argv[1]}@dns.lezts.mongodb.net/?retryWrites=true&w=majority`;
-    fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(config));
-    console.log(config)
+    try {
+        const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')).toString());
+        config.dbUrl = `mongodb+srv://${argv[0]}:${argv[1]}@dns.lezts.mongodb.net/?retryWrites=true&w=majority`;
+        fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(config));
+        console.log("Successfully saved your mongodb login and password data: \n", config)
+    } catch (err) {
+        console.error(err);
+        console.log('\n failed to save your login or password');
+    }
 } else {
     console.log('wrong syntax');
 }
